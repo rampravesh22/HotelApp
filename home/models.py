48647ff9095bf3eeb6b77ccf6da506ed3e04977c
rpc <rpc7863@gmail.com>
2022-07-12 18:56:24 +0530

    added

--- home/models.py
+++ home/models.py
@@ -1,3 +1,42 @@
 from django.db import models
-
+import uuid
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class BaseModel(models.Model):
+    uid = models.UUIDField(
+        default=uuid.uuid4, editable=False, primary_key=True)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now_add=True)
+    class Meta:
+        abstract = True
+
+
+class Amenities(BaseModel):
+    amenity_name = models.CharField(max_length=100)
+
+
+class Hotel(BaseModel):
+    amenities = models.ManyToManyField(Amenities)
+    hotel_name = models.CharField(max_length=100)
+    hotel_price = models.FloatField()
+    description = models.TextField()
+    root_count = models.IntegerField(default=10)
+
+
+class HotelImages(BaseModel):
+    hotel = models.ForeignKey(
+        Hotel, related_name="hotel_images", on_delete=models.CASCADE)
+    images = models.ImageField(upload_to="hotel")
+
+
+class HotelBooking(BaseModel):
+    hotel = models.ForeignKey(
+        Hotel, related_name="hotel_booking", on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User, related_name="user_booking", on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    booking_type = models.CharField(max_length=100,
+        choices=(("Pre paid", 'prepaid'), ("Post paid", 'postpaid')))
